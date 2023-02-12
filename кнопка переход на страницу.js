4a28b0bab2b89d68aebea0e27e927c8d1631df4c
Nata3951 <Nata3951@gmail.com>
2023-02-12 20:26:15 +0300

    Update кнопка переход на страницу.js

--- кнопка переход на страницу.js
+++ кнопка переход на страницу.js
@@ -1,3 +1,5 @@
+// VERSION 1
+
 TextRender({
     text: w.general,
     style: w.style
@@ -32,3 +34,22 @@ $container.on('click', function(){
             }
     });
 
+
+// VERSION 2
+
+ImageRender({
+    image: w.general
+});
+
+$('#' + w.general.renderTo).hover(
+    function mouseenter() {
+        $('#' + w.general.renderTo).children().css('background', 'url("https://rosseti.polyanalitika.ru/corelogic/api/query/image?fileGuid=4acc84b4fb894287838c97e143827c39&access_token=NoAuth") center center / contain no-repeat')
+    },
+    function mouseleave() {
+        $('#' + w.general.renderTo).children().css('background', 'url("https://rosseti.polyanalitika.ru/corelogic/api/query/image?fileGuid=4569d6f8b6ba4210ae4fad8c37375dd0&access_token=NoAuth") center center / contain no-repeat')
+    }
+)
+
+$('#' + w.general.renderTo).click(function() {
+    window.top.location.href  = "https://rosseti.polyanalitika.ru/dashboardsViewer?sectionId=7&dashboardId=f9774c12ff6e4fa68c1ed08ca5acc633&sheetId=091d585351444ed2bacbcc974999e97f"
+})
