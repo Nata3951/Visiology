0b2bf39488738d6e5f7d32f749d5aad3652c93c2
Nata3951 <Nata3951@gmail.com>
2023-03-29 14:06:40 +0300

    Update and rename 230326 RS.md to 230326 RS textbox border + align.js

---
+++ backup/230326 RS textbox border + align.js
@@ -0,0 +1,10 @@
+$('#widget-' + w.general.renderTo).css({
+    'border-right': "2px solid #494b6f",
+});
+
+// align text at bottom
+$('#widget-' + w.general.renderTo + ' > div.va-widget-body-container > div').css({
+    'position' : 'absolute',
+    'height' : 'auto',
+    'bottom' : 0,
+});
