fcbd06dcbb42371a2af043bd08de524979949b7b
Nata3951 <Nata3951@gmail.com>
2023-04-30 21:29:36 +0300

    Update 230430 xvm сети page1 table formatted.js

--- backup/230430 xvm сети page1 table formatted.js
+++ backup/230430 xvm сети page1 table formatted.js
@@ -20,9 +20,96 @@ $("#table-"+w.general.renderTo+" > thead > tr:nth-child(1) > th:nth-child(1), #t
 
 // Добавляем/заменяем заголовки
 $("#table-"+w.general.renderTo+" > thead > tr:nth-child(1) > th:nth-child(1) > div > span:nth-child(1)").text('Показатель');
-$("#table-"+w.general.renderTo+" > thead > tr:nth-child(1) > th:nth-child(2) > div > span:nth-child(1)").text('Ед. измерения');
+$("#table-"+w.general.renderTo+" > thead > tr:nth-child(1) > th:nth-child(2) > div > span:nth-child(1)").text('Ед. изм.');
 $("#table-"+w.general.renderTo+" > thead > tr:nth-child(1) > th:nth-child(3) > div > span:nth-child(1)").text('Факт 2017'); 
 $("#table-"+w.general.renderTo+" > thead > tr:nth-child(1) > th:nth-child(8) > div > span:nth-child(1)").text('Бизнес план 2030');
+
+// добавляем высоту строк
+$('#table-' + w.general.renderTo + ' tbody > tr > td').css({
+//    'margin-block-start': '0',
+    /* top | right | bottom | left */
+    'padding':'50px 0px 50px 15px',
+});
+
+// ширина колонок
+$('#table-' + w.general.renderTo + ' th:nth-child(1)')
+.css({
+    "width":"240px"
+});
+
+$('#table-' + w.general.renderTo + ' th:nth-child(2)')
+.css({
+    "width":"110px"
+});
+
+// раскраска ячеек факта по сравнию с планом
+// номер колонки с фактом = [4,6]
+// selector для строка 2, ячейка 4
+// #table-681806a576554d5cb4c51970abc268a3 > tbody > tr:nth-child(2) > td:nth-child(4)
+
+
+function chooseColor(v) {
+    if (v < 0) return '#ff595a';
+    else return '#56ad83';
+}
+
+/* функция для раскраски колонки факта. Задаем номер ряда и направление оценки 
+(1 если лучше, когда факт меньше плана и -1 если наоборот) */
+
+function colorFactColumn(measureRow, direction) {
+    factColumns.forEach(function(j) {
+        $(`#table-681806a576554d5cb4c51970abc268a3 > tbody > tr:nth-child(${measureRow}) > td:nth-child(${j})`).each(
+            function(i, td) {
+                let fact = $(`#table-681806a576554d5cb4c51970abc268a3 > tbody > tr:nth-child(${measureRow}) > td:nth-child(${j})`)[0].innerText;
+                let plan = $(`#table-681806a576554d5cb4c51970abc268a3 > tbody > tr:nth-child(${measureRow}) > td:nth-child(${j+1})`)[0].innerText;
+                let diff = (plan-fact)*direction;
+                
+                $(td).css({
+                    'color': chooseColor(diff),
+                });
+        });
+    }); 
+}
+
+let factColumns = [4,6];
+
+
+colorFactColumn(1, -1);
+colorFactColumn(2, 1);
+colorFactColumn(3, -1);
+
+
+
+// раскраска зеброй, четные строки
+$('#table-' + w.general.renderTo + ' tr:nth-child(even) td').css({
+    'background-color': '#3f4268', 
+});
+
+// раскраска зеброй, нечетные строки
+$('#table-' + w.general.renderTo + ' tr:nth-child(odd) td').css({
+    'background-color': '#494b6f', 
+});
+
+
+function numberWithSpaces(x) {
+  var parts = x.toString().split(".");
+  let head = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  let tail = parts[1] ? (parts[1]*100 +"").slice(0,2) : "00";
+  return head +','+ tail;
+}
+
+// для цифровых колонок добавим разделители разрядов, две цифры после запятой, отбивку справа
+[3,4,5,6,7,8].forEach(function(j) {
+    $('#table-' + w.general.renderTo + ' tr > td:nth-child(' + j + ')').each(function(i, td) {
+        var value = +td.innerHTML;
+        td.innerText = numberWithSpaces(td.innerText); 
+        $(td).css({
+            'text-align': 'right',
+            'padding-right':'15px',
+        });
+}); 
+ 
+});
 
 
 
