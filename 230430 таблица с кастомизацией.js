c109af29b67c477e5ada940268a39f6e66a8e941
Nata3951 <Nata3951@gmail.com>
2023-05-01 11:25:59 +0300

    Update 230430 таблица с кастомизацией.js

--- 230430 таблица с кастомизацией.js
+++ 230430 таблица с кастомизацией.js
@@ -229,8 +229,9 @@ function numberWithSpaces(x) {
     }
 }
  
+//отступ слева в заголовке виджета
     $('#widget-' + w.general.renderTo + ' div.va-widget-header-container').css({
-    'padding-left': '30px'  //отступ слева в титуле виджета
+    'padding-left': '30px'  
 }); 
 
 
