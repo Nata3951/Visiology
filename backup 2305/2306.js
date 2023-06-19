06e8d88c073062bc241de6cc394a3e64b4e9f9c2
Nata3951 <Nata3951@gmail.com>
2023-06-19 20:36:25 +0300

    Update 2306.js

--- backup 2305/2306.js
+++ backup 2305/2306.js
@@ -1,3 +1,8 @@
 // поджать ось, чтобы бар занимал больше места на графике 
 w.yAxis.OnTick = false;
 w.yAxis.endOnTick = false;
+
+// сделать легенду некативной
+$('#' + w.general.renderTo).find('.highcharts-legend').css({ 
+    'pointer-events': 'none',
+   });
