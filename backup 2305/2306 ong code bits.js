928e55a383b5fcc4fa7d17c16716fa2ebca45660
Nata3951 <Nata3951@gmail.com>
2023-07-10 16:29:21 +0300

    Update 2306 ong code bits.js

--- backup 2305/2306 ong code bits.js
+++ backup 2305/2306 ong code bits.js
@@ -15,3 +15,12 @@ w.yAxis[0].labels.style.color = '#1c4680';
 w.yAxis[1].labels.style.color = '#4db6ac';
 w.yAxis[1].labels.style.fontFamily = "Open Sans";
 w.yAxis[1].labels.style.fontSize = "12px";
+
+
+// рассчитаем разницу между второй и первой серией
+w.series[0].data.forEach((el, ind) => el.y = w.series[1].data[ind].y - w.series[0].data[ind].y);
+// удалим ненужную серию
+w.series.pop();
+
+// зададим цвет отрицательных значений
+w.series[0].negativeColor = '#ff1744';
