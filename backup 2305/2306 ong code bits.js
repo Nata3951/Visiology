40fe7a54c5283b824ca5a38a92cc53904901758a
Nata3951 <Nata3951@gmail.com>
2023-07-10 10:55:54 +0300

    primary secondary Y axis color

--- backup 2305/2306 ong code bits.js
+++ backup 2305/2306 ong code bits.js
@@ -6,3 +6,7 @@ w.yAxis.endOnTick = false;
 $('#' + w.general.renderTo).find('.highcharts-legend').css({ 
     'pointer-events': 'none',
    });
+
+// цвет шрифта первичной и вторичной оси Y
+w.yAxis[0].labels.style.color = '#1c4680';
+w.yAxis[1].labels.style.color = '#4db6ac';
