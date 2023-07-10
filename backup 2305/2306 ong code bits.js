3d30f31b69e2964e316dbc5067b84505a5450bf3
Nata3951 <Nata3951@gmail.com>
2023-07-10 11:21:40 +0300

    Update 2306 ong code bits.js

--- backup 2305/2306 ong code bits.js
+++ backup 2305/2306 ong code bits.js
@@ -7,6 +7,8 @@ $('#' + w.general.renderTo).find('.highcharts-legend').css({
     'pointer-events': 'none',
    });
 
-// цвет шрифта первичной и вторичной оси Y
+// цвет и настройки шрифта первичной и вторичной оси Y
 w.yAxis[0].labels.style.color = '#1c4680';
 w.yAxis[1].labels.style.color = '#4db6ac';
+w.yAxis[1].labels.style.fontFamily = "Open Sans";
+w.yAxis[1].labels.style.fontSize = "12px";
