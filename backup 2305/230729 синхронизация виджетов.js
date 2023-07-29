69e1fe5a9f0239aa01d04b4e62aa7744f550c6a5
Nata3951 <Nata3951@gmail.com>
2023-07-29 17:55:58 +0300

    Update 230729 синхронизация виджетов.js

--- backup 2305/230729 синхронизация виджетов.js
+++ backup 2305/230729 синхронизация виджетов.js
@@ -54,3 +54,66 @@ TextRender({
     text: w.general,
     style: w.style
 });
+
+
+// 
+const filterOneGuId = '204f7adb0a664500a1aece2e7e4629d9';
+let selected = w.data.selected;
+
+setTimeout(() => {filterAndRender()}, 500)
+visApi().onSelectedValuesChangedListener({guid: filterOneGuId, widgetGuid: filterOneGuId }, function (info) {filterAndRender(info)})
+
+
+function filterAndRender(info) {
+    const wDup = JSON.parse(JSON.stringify(w));
+    let selectedValue = (info?info.selectedValues:visApi().getSelectedValues(filterOneGuId))[0][0];
+    
+    // добавляем дату на год меньше от последней
+    let selectedValueArr = selectedValue.split(' ');
+    
+    // ищем дату на год отличающуюся от последней доступной
+    wDup.data.data.map(el => {
+        let elArr = el.text.split(' ');
+        
+        
+        if (selectedValueArr.length === 2 && elArr.length === 2) {
+            if (selectedValueArr[0] === elArr[0] && (Number(selectedValueArr[1]) - 1) === Number(elArr[1])) {
+                selected[0] = el;
+            }
+        } else if (selectedValueArr.length === 3 && elArr.length === 3) {
+            if (selectedValueArr[0] === elArr[0] && selectedValueArr[1] === elArr[1] && (Number(selectedValueArr[2]) - 1) === Number(elArr[2])) {
+                 selected[0] = el;
+            }
+        }
+    });
+
+    
+    FilterRender({
+        filter: wDup.general,
+        style: wDup.style,
+        textStyle: wDup.textStyle,
+        data: {...wDup.data, selected}
+    }); 
+    
+    getStyle();
+}
+
+// 
+
+// фильтры, которые слушаем
+let filterGuId = ['9f2e3d4a28ba442faef7dcc577b0543f'];
+
+// виджеты, в которых меняем сообщение об ошибке
+let errorVidgetGuid = ['b36d8e3a91044128b4e4891464184c7c'];
+
+// треугольник, который выдается в сообщении об ошибке
+let triangle = $('#va-widget-error-'+ errorVidgetGuid + ' > i');
+
+//  сообщение об ошибке
+let text = $('#va-widget-error-'+ errorVidgetGuid + ' > i > div');
+
+
+// нужный формат
+triangle.css({ "color":"transparent"});
+text.css({ "color":"gold"}); 
+text[0].innerText = 'some other text';
