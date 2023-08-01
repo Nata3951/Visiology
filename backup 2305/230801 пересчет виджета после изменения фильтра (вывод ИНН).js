4c0b9e8f83e3d354c8dad84dec81ef12c6e3c6b5
Nata3951 <Nata3951@gmail.com>
2023-08-01 11:23:04 +0300

    Update and rename 230801 пересчет виджета после изменения фильтра.js to 230801 пересчет виджета после изменения фильтра (вывод ИНН).js

--- backup 2305/230801 пересчет виджета после изменения фильтра (вывод ИНН).js
+++ backup 2305/230801 пересчет виджета после изменения фильтра (вывод ИНН).js
@@ -1,4 +1,6 @@
-const filterGuid = '6a680a9c78b44ecaa2c7937ba28f32b3';
+const filterGuid = '6a680a9c78b44ecaa2c7937ba28f32b3'; // Контрагент
+// console.log('test ', w);
+
 visApi().onSelectedValuesChangedListener({ guid: w.general.renderTo, widgetGuid: filterGuid }, render);
 
 function render() {
@@ -6,7 +8,9 @@ function render() {
   const valueName = selectedValues.length ? selectedValues[0][0] : '';
   const targetRowIndex = w.data.rows.findIndex(el => el[0] === valueName);
   const valueSecondName = (targetRowIndex + 1) ? w.data.rows[targetRowIndex][1] : '';
-
+  const valueINN = ((targetRowIndex > 0) && /\d/.test(w.data.rows[targetRowIndex][2])) ? w.data.rows[targetRowIndex][2] : 'отсутствует';
+  
+  
   const oniTotalMap = {
     'Авиация': w.data.values[1][1],
     'Авто и ДСТ': w.data.values[1][2],
@@ -15,9 +19,13 @@ function render() {
     'Прочее': w.data.values[1][5],
     'Цифровые активы': w.data.values[1][6],
   };
-if (w.data.values[1].length >= 8) {
+if (targetRowIndex > 0) {
   const oniTotal = oniTotalMap[w.data.rows[7][1]] || w.data.values[1][0];
-  w.general.text = `<span style="color:#212121;font-weight:bold;padding-left:0px">${valueName}</span> | <span style="color:#212121;font-weight:bold">${valueSecondName}</span> | Доля в портфеле отрасли: <span style="color:#212121;font-weight:bold">${(w.data.values[0][7] / oniTotal * 100).toFixed(2).split('.').join(',')}%</span>`;
+  w.general.text = `
+    <span style="color:#212121;font-weight:bold;padding-left:0px">${valueName}, ИНН ${valueINN}</span> 
+    | <span style="color:#212121;font-weight:bold">${valueSecondName}</span> 
+    | Доля в портфеле отрасли: <span style="color:#212121;font-weight:bold">${(w.data.values[0][7] / oniTotal * 100).toFixed(2).split('.').join(',')}%</span>
+    `;
 } else {
 w.general.text = `<span style="color:#212121;font-weight:bold;padding-left:0px">${valueName}</span>`; 
 }
