f07563f0484fc1a2703a5a4630c1cf8bc154785e
Nata3951 <Nata3951@gmail.com>
2023-08-03 09:37:44 +0300

    Update 230802 обрезка названия.js

--- backup 2305/230802 обрезка названия.js
+++ backup 2305/230802 обрезка названия.js
@@ -14,9 +14,19 @@ w.general.text = `<span style="color:#212121;font-weight:bold;padding-left:0px">
     style: w.style,
   });
 
- sns.scatterplot(
-        x=df['pdz_forecast'], y=df['pdz_now'], hue=df['rating'], palette = 'bwr_r', 
-     s = 300, alpha = 0.5,
-     legend=False,     
+---
+ind = 'Водный транспорт'
+temp = df.query("industry == @ind")
+temp1 = df.query("industry == @ind & pdz_forecast > 500").reset_index()
+
+g = sns.relplot(data = temp,
+        x='pdz_forecast', y='pdz_now', hue='rating', palette = sns.color_palette("blend:grey,gold,coral,red", as_cmap=True), 
+     size='pdz_forecast', alpha = 0.9,  sizes = (10,400),
+     legend=False, height = 3
     )
-plt.show()
+g.set(xlim=(0, 1900), ylim=(0, 2000))
+
+for i, client in enumerate (temp1['client']):
+    plt.text(temp1['pdz_forecast'][i]-600, temp1['pdz_now'][i]+200, client)
+
+display(temp1)
