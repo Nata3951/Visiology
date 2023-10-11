3032102b5c6501db9a2fe5e9f3141f765c82f874
Nata3951 <Nata3951@gmail.com>
2023-10-11 10:22:45 +0300

    Update and rename 231011 таблица с многоуровневой шапкой.js to 231011 промо - таблица с многоуровневой шапкой.js

--- backup 2309/231011 промо - таблица с многоуровневой шапкой.js
+++ backup 2309/231011 промо - таблица с многоуровневой шапкой.js
@@ -46,22 +46,50 @@ $(`#table-${w.general.renderTo} th:contains("актические")`)
 
 
 
-
-
-
-
-    
 // шрифт шапки
 $(`#table-${w.general.renderTo} th`).css({
-    "font-weight":"normal"
+    "font-weight":"normal",
+    'padding-left' : '7px',
 });
 
+// форматируем текст шапки 
+$('#table-'+w.general.renderTo+' > thead  div span').css({ 
+    'text-align' : 'left',
+    'word-break' : 'normal',
+});
+
+// скрываем маркеры сортировки
+$(`#table-${w.general.renderTo} th .fa-sort`).css({ 
+        "color" : "transparent",
+});
 
 // настраиваем колонки
 $('#table-' + w.general.renderTo + ' th:nth-child(1)').css({
-    "width":"14%"
+    "width":"13%"
 });
 
 $('#table-' + w.general.renderTo + ' th:last-child').css({
     "width":"8%"
+});
+
+// выравниваем числовые ячейки по правому краю
+$(`#table-${w.general.renderTo} td`)
+    .not(":last-child")
+    .not(":first-child")
+    .css({ 
+        "text-align" : "right",
+        "padding-right" : '10px',
+});
+
+// формат текста заголовков строк
+$(`#table-${w.general.renderTo} td:first-child`)
+    .css({ 
+        "font-weight" : "600",
+});
+
+// подсветим последнюю строку
+$(`#table-${w.general.renderTo} tbody tr:last-child`)
+    .css({ 
+        "background-color" : "#2196f352",
+        // "font-weight" : "600",
 });
