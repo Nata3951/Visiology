34461b5e95ab4adc27219f5a0588907a067aecaa
Nata3951 <Nata3951@gmail.com>
2023-10-13 11:26:00 +0300

    Update 231012 template prez.js

--- backup 2309/231012 template prez.js
+++ backup 2309/231012 template prez.js
@@ -1,4 +1,17 @@
-let background =  '#18324b'
+let background =  '#18324b';
+let header_font_spec = 'bold 22px Open Sans';
+// светофор
+let red = '#ff5252';
+let orange = '#ffb300';
+let yellow = '#ffd740';
+let green = '#aed581';
+// таблица 
+let row_light = '#293c52';
+let row_dark = '#1e3249';
+// текст 
+let text1 = '#fafafa';
+let text2 = '#9e9e9e';
+// график 
 let azure = "#2196f3";
 let orange_b = "#ff9800";
 let turq = '#00bcd4';
@@ -6,22 +19,17 @@ let turq2 = '#3d90a8';
 let purple = '#9c27b0';
 let purple2 = '#6b328d';
 let lime = '#cddc39';
-let text1 = '#fafafa';
-let text2 = '#9e9e9e';
-// светофор
-let red = '#ff5252';
-let orange = '#ffb300';
-let yellow = '#ffd740';
-let green = '#aed581';
+
+
+
 
 // bar chart
 w.yAxis.OnTick = false; 
 w.yAxis.endOnTick = false;
 w.plotOptions.series.borderColor = 'transparent';
-w.series[0].color = turq2;
-w.series[1].color = azure;
-w.series[2].color = orange_b;
-
+w.tooltip.crosshairs = true;
+w.tooltip.shared = true;
+w.plotOptions.series.lineWidth = 4;
 
 //  фон виджета
  $(`#widget-${w.general.renderTo}`).css({
@@ -47,3 +55,17 @@ $(`#widget-${w.general.renderTo} .va-widget-header`).css({
     'font' : 'bold 22px Open Sans',
     'color' : text1
 });
+
+// ТАБЛИЦА
+// заголовок таблицы
+$('#table-' + w.general.renderTo + ' thead th')
+.css("border","1px solid rgb(200,209,215)")
+.css("color", text1)
+.css("padding","15px 5px");
+
+// ячейки таблицы: шрифт
+$(`#table-${w.general.renderTo} td`)
+    .css({
+        'font' : '18px Open Sans',
+        'color' : text1,
+    });
