09b17752dab80a6994cda5ac417a285d521cc77e
Nata3951 <Nata3951@gmail.com>
2023-10-16 15:16:36 +0300

    Update 231016 sort out.js

--- backup 2309/231016 sort out.js
+++ backup 2309/231016 sort out.js
@@ -1,7 +1,15 @@
-        dataLabels: {
+dataLabels: {
 
                 formatter: function () {
                                 if (this.point.value !== 0) {
                                 return this.point.value;
                                 }
                             return null;
+
+
+// pie chart series colors
+
+w.series[0].data[0].color = '#2196f3';
+w.series[0].data[1].color = '#ff9800';
+w.series[0].data[2].color = '#00bcd4';
+w.series[0].data[3].color = '#9c27b0';
