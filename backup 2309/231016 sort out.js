b602c52b3e2f9d094934f5772da70ca34f63c133
Nata3951 <Nata3951@gmail.com>
2023-10-16 11:58:19 +0300

    Create 231016 sort out.js

---
+++ backup 2309/231016 sort out.js
@@ -0,0 +1,7 @@
+        dataLabels: {
+
+                formatter: function () {
+                                if (this.point.value !== 0) {
+                                return this.point.value;
+                                }
+                            return null;
