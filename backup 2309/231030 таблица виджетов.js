717f04fe275022977aeed65b97960e173abf4830
Nata3951 <Nata3951@gmail.com>
2023-10-30 18:42:15 +0300

    Update 231030 таблица виджетов.js

--- backup 2309/231030 таблица виджетов.js
+++ backup 2309/231030 таблица виджетов.js
@@ -1,3 +1,6 @@
+let index_of_type = w.data.rowHeaders.indexOf('widget_type');
+let index_of_name = w.data.rowHeaders.indexOf('widget_name');
+let index_of_link = w.data.rowHeaders.indexOf('widget_link');
 
 
 
@@ -6,7 +9,7 @@ let mainDivId = w.general.renderTo;
 let mainDiv = document.getElementById(mainDivId);
 
 
-
+// зададим стили для классов
 
 mainDiv.innerHTML = `
     <style>
@@ -18,8 +21,10 @@ mainDiv.innerHTML = `
             width: 100%;
             text-align: left;
             font-size: 30px;
-            padding: 20px;
-            background-color: pink;
+            padding-top: 20px;
+            padding-bottom: 0px;
+            padding-left:20px;
+            // background-color: pink;
         }
         
         .image-container {
@@ -44,42 +49,63 @@ mainDiv.innerHTML = `
     </style>
 `;
 
-// Тип виджета / Название виджета / 
 
+
+
+
+
+// получим список уникальных типов виджетов
 let widget_types = new Set();
-let index_of_type = w.data.rowHeaders.indexOf('widget_type');
+
+
 w.data.rows.forEach((el, ind) => widget_types.add(el[index_of_type]) );
 
-console.log('test array', Array.from(widget_types));
+// создадим раздел для каждого типа виджетов
+Array.from(widget_types).forEach((w_type,ind1) => { 
 
-// Array.from(widget_types).forEach((el, ind) => console.log('test el', el));
-
-
-Array.from(widget_types).forEach((el,ind) => { 
-
-    let headerId = 'header'+ind;
+    let headerId = 'header'+ind1;
     let header = createElem('div', headerId, ['title'], mainDivId);
-    header.innerHTML = el;
+    header.innerHTML = w_type;
     
-    let imageContainerId = 'image-container'+ind;
+// создадим контейнер для изображений в каждом разделе
+    let imageContainerId = 'image-container'+ind1;
     let imageContainer = createElem('div', imageContainerId, ['image-container'], mainDivId);
-    
-    for (let i = 0; i < 5; i++) {
+
+
+// отфильтруем виджеты текущего типа
+
+    let data_by_type = w.data.rows.filter((el) => el[index_of_type] == w_type);
+
+
+// поместим в контейнер изображения и подписи для каждого виджета в таблице 
+    // console.log('test z', data_by_type);
+
+    data_by_type.forEach ((row, ind2) => {
         
-        let tmpImgContainerId = 'image-container-lvl2-' + ind + i;
+        let tmpImgContainerId = 'image-container-lvl2-' + ind1 + ind2;
         let tmpImgContainer = createElem('div', tmpImgContainerId, [], imageContainerId);
         
-        let image = createElem('img', 'img-' + ind + i, [], tmpImgContainerId);
+        let imageID = 'img-' + ind1 + ind2;
+        let image = createElem('img', imageID, [], tmpImgContainerId);
         image.setAttribute('src', 'https://img-fotki.yandex.ru/get/96333/10235337.3/0_15dec3_1535c4e_orig.jpg');
-        
-        let linkID = 'a-' + ind + i;
+
+        // $(`#${imageID}`).css({ 
+        //     'width' : '100%', 
+        //     'height': '100px', 
+        //     'background' : "https://forjs.polyanalitika.ru/corelogic/api/query/image?fileGuid=7b6a6025d4284bb0b535a7538f5f533d&access_token=NoAuth"
+        // });
+
+        let linkID = 'a-' + ind1 + ind2;
         let link = createElem('a', linkID, [], tmpImgContainerId);
-        let label = createElem('label', 'label-'+ ind + i, [], linkID);
-        label.innerHTML = 'Widget '+ ind + i  ;  
-    }
+        link.setAttribute('href', row[index_of_link]);
+        
+        let label = createElem('label', 'label-'+ ind1 + ind2, [], linkID);
+        label.innerHTML = row[index_of_name];  
+    });
 }
 );
 
+    console.log('test w', w);
 
 
 
@@ -101,3 +127,4 @@ function createElem(elemType, elemID, classes, parentID) {
     
     return newElem;
 }
+
