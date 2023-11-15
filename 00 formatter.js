6e8cbc58d7a6babf2dff401275b6ca5aea54861e
Nata3951 <Nata3951@gmail.com>
2023-11-15 19:39:12 +0300

    Update 00 formatter.js

--- 00 formatter.js
+++ 00 formatter.js
@@ -5,4 +5,29 @@ w.xAxis.labels.formatter = function(){
 }
 
 
+// ## Tooltip formatting
+
+w.tooltip.formatter = function () {
+    var s = '<b>' + this.x + '</b>';
+
+    $.each(this.points, function () {
+        var symbol = '●';
+        if (this.series.name != "area")
+        s += '<br/>' + '<span style="color:' + this.series.color + '">' + symbol + '</span>' + ' ' + this.series.name + ': ' + this.y;
+    });
+
+    return s;
+};
+
+
+// using formatter
+
+w.plotOptions.series.dataLabels.formatter = function() {
+    return this.y + '%';
+};
+
+w.plotOptions.series.dataLabels.formatter = function() {
+    console.log('test this', this);
+    return this.y + '%';
+};
 
