f140d62ef4ba3b8265dd8e37da072cab842e8efa
Nata3951 <Nata3951@gmail.com>
2023-11-16 09:57:26 +0300

    Update 231016 sort out.js

--- backup 2311/231016 sort out.js
+++ backup 2311/231016 sort out.js
@@ -1,3 +1,13 @@
+// x axes no rotate + formatter
+
+w.xAxis.labels.autoRotation = undefined;
+w.xAxis.labels.formatter = function(){
+    console.log('test', this.value)
+    if (this.value.includes('усто')) return 'Не определено из 1С';
+    return this.value
+}
+
+
 // insert with jquery
 $('<div>', {
     id: 'id10',
