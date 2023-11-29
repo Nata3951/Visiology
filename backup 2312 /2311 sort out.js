9000c565d7624010a71e67dbdf91a57a66c96585
Nata3951 <Nata3951@gmail.com>
2023-11-29 17:52:17 +0300

    Update 2311 sort out.js

--- backup 2312 /2311 sort out.js
+++ backup 2312 /2311 sort out.js
@@ -1,1 +1,10 @@
+let other = {};
+other.name = 'Остальное';
+console.log('test other', other);
 
+let other_value = 0; 
+
+for (let i = 6; i < 8; i++) {
+    w.series[i].data.forEach((el) => if(el.y) {x += el.y})  
+    console.log('test value', x);
+}
