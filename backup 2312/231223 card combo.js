72be71634a614e291732890605457635800637c3
Nata3951 <Nata3951@gmail.com>
2023-12-25 14:36:23 +0300

    Update 231223 card combo.js

--- backup 2312/231223 card combo.js
+++ backup 2312/231223 card combo.js
@@ -1,7 +1,11 @@
 let blue_dark = '#1c4680';
 let blue = '#8da2bf';
-let red = '#ff8a80';
+let color_main = '#ff8a80'; // red
 let white = '#f5f5f5';
+let font_grey = '#757575';
+
+let rating = 'D';
+let pl = '15px' // padding left
 
 let total_arr = w.data.values[0];
 let filtered_arr = w.data.values[1];
@@ -10,96 +14,172 @@ let total = total_arr.reduce((acc, val) => acc + val, 0 );
 let filtered = filtered_arr.reduce((acc, val) => acc + val, 0 ) ?? 0;
 let share = filtered / total * 100;
 
+// найдем индекс первого прогноза
+let forecast_start = w.data.rows.findIndex(el => el[2] == 'прогноз');
+
+// посчитаем ближайший прогноз по выбранному рейтингу
+let pdz_forecast = filtered_arr[forecast_start];
+
+let pdz_pct;
+let pct;
+
+if (filtered_arr[forecast_start-1]  && pdz_forecast) { 
+    pct = (pdz_forecast / filtered_arr[forecast_start-1] - 1)*100;
+    let sign =  pct > 0 ? '+' : '';
+    pdz_pct = sign+pct.toFixed(0)+'%';
+}
+else pdz_pct = 'н/д';
+
+let pct_color = pct < 0 ?'green' : 'tomato';
+
+
+
 console.log ('test w', w)
+console.log ('test f', pdz_pct)
+
+
+
+function numberWithSpaces(value, points=0, div='') { 
+    if(!value) return "";
+    let parts = value.toString().split(".");
+    let head = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    let tail = parts[1] ? (parts[1]+"0000000").slice(0,points) : "0".repeat(points);
+    return head+div+tail;
+}
+
 
 let mainDiv = document.getElementById(w.general.renderTo);
 
-// зададим стили для классов
-mainDiv.innerHTML = `
-    <style>
-    
-    .title1 {
-    // background-color: lavender ;
-    height : 22%;
-    width: 80%;
-    padding-left: 10px;
-    padding-top: 10px;
-    }
-    
-    .hbar1 {
-    // background-color: lavender ;
-    height : 28%;
-    width: 80%;
-    }
-    
-    .cont2 {
-        // background-color: peachpuff ;
-        height : 50%;
-        display: flex;
-    }
-    
-    .card1 {
-    background-color: gold;
-    height: 100%;
-    width : 60%;
-    }
-    
-    .line1 {
-    height: 100%;
-    width : 40%;
-    }
-    
-    
-    </style>
-`;
-
 // создадим контейнеры для графиков
 
-     $('<div>', {
-        class: 'title1',
-        id: 'title123'
-    })
-    .appendTo(mainDiv);
- 
+// верхний блок
     $('<div>', {
-        class: 'hbar1',
-        id: 'hbar123'
+        id: `cont1-${w.general.renderTo}`
+    })
+    .css ({
+        'height' : '50%',
+        // 'background-color' : 'gold',
+        'display' : 'flex',
+        'padding-left' : `${pl}`,
     })
     .appendTo(mainDiv);
 
+// container for title and bar
+
     $('<div>', {
-        class: 'cont2',
+    id: `cont1_1-${w.general.renderTo}`
+    })
+    .css ({
+        height : '100%',
+        width : '80%',
+        // 'background-color' : 'goldenrod',
+    })
+    .appendTo($(`#cont1-${w.general.renderTo}`));
+
+
+// title
+     $('<div>', {
+        class: 'title',
+        id: `title-${w.general.renderTo}`
+    })
+    .css ({
+    // backgroundColor: 'lavender',
+    height : `45%`,
+    width: '100%',
+    'padding-top': '10px',
+    })
+    .html(`
+      <span style="font-weight:bold;"> ${share.toFixed()}% </span> 
+      <span style="color:${font_grey}"> доля в общем прогнозе </span>
+      `)
+    .appendTo($(`#cont1_1-${w.general.renderTo}`));
+
+// bar 
+    $('<div>', {
+        class: 'hbar',
+        id: `hbar-${w.general.renderTo}`
+    })
+    .css ({
+    // backgroundColor: 'lavender',
+    height : `55%`,
+    width: '100%',
+    })
+    .appendTo($(`#cont1_1-${w.general.renderTo}`));
+    
+// container for rating
+
+$('<div>', {
+id: `cont1_2-${w.general.renderTo}`
+})
+.css ({
+    height : '50%',
+    width : '14%',
+    backgroundColor : white,
+    margin : '7% 3% 4% 6%',
+    borderRadius : 5,
+    textAlign : 'center',
+    fontSize : 28,
+    fontWeight : '600',
+    color : color_main,
+})
+.text(rating)
+.appendTo($(`#cont1-${w.general.renderTo}`));
+
+// нижний блок
+    $('<div>', {
+        id: `cont2-${w.general.renderTo}`
+    })
+    .css ({
+    'height' : '50%',
+    // 'background-color' : 'gold',
+    'display' : 'flex',
+    'padding-left' : `${pl}`,
     })
     .appendTo(mainDiv);
-    
+
+// карточка
     $('<div>', {
-        class: 'card1',
+        id: `card-${w.general.renderTo}`
     })
-    .appendTo($(`.cont2`));
-    
+    .css ({
+        // backgroundColor: 'plum',
+        height : '100%',
+        width: '47%',
+        'padding-top' : 10,
+    })
+    .html(`
+        <span style="font-size:200%; font-weight:bold"> ${numberWithSpaces(pdz_forecast,2, ',')} </span>
+        <span style="color:${pct_color}; font-weight:bold">${pdz_pct}</span></br>
+        <span style="color:${font_grey}">Прогноз ПДЗ, млн руб.</span
+        `)
+    .appendTo($(`#cont2-${w.general.renderTo}`));
+
+
+// container for line chart 
     $('<div>', {
-        class: 'line1',
-        id: 'line123',
+         id: `line-${w.general.renderTo}`,
     })
-    .appendTo($(`.cont2`));
-    
-// создадим надпись
-  $('.title1').html(`<span style="font-weight:bold;"> ${share.toFixed()}% </span> доля в общем прогнозе`);
+    .css ({
+        // backgroundColor: 'lime',
+        height : `100%`,
+        width: '53%',
+    })
+    .appendTo($(`#cont2-${w.general.renderTo}`));
     
 // отрисуем bar chart
 Highcharts.chart({
     chart: {
         type: 'bar',
-        renderTo: hbar123,
-        margin: [0, 0, 15,10],
+        renderTo: `hbar-${w.general.renderTo}`,
+        margin: [0, 0, 15,0],
     },
 
     xAxis: {
         visible: false,
-        lineWidth : 0,
+        maxPadding : 0,
+        minPadding : 0,
         onTick: false,
         endOnTick:false,
-        maxPadding : 0,
 
     },
     yAxis: {
@@ -125,7 +205,7 @@ Highcharts.chart({
                 align : 'right',
                 x : -10,
                 formatter : function () {
-                  return this.y.toFixed();
+                  return numberWithSpaces(this.y);
                 },
                 
             }
@@ -141,7 +221,7 @@ Highcharts.chart({
         name: 'PDZ filtered',
         data: [filtered],
         pointPadding : 0,
-        color : red,
+        color : color_main,
     }]
 });
 
@@ -150,7 +230,7 @@ Highcharts.chart({
 Highcharts.chart({
     chart: {
         type: 'area',
-        renderTo: line123,
+        renderTo: `line-${w.general.renderTo}`,
         margin: [0, 10, 10, 10],
     },
 
@@ -172,6 +252,9 @@ Highcharts.chart({
         data: filtered_arr,
         color : blue,
         fillOpacity : 0.2,
+        marker : {
+            enabled : false,
+        },
     }]
 });
 
@@ -179,5 +262,5 @@ Highcharts.chart({
     $(`#${w.general.renderTo}`).css({
         'font-size' : '13px',
         'font-family': 'Open Sans',
-        'background': 'linear-gradient(0.25turn, #ff8a8066, #8da2bf33, #8da2bf66)',
+        'background': `linear-gradient(0.25turn, ${color_main + '33'}, #8da2bf33, #8da2bf66)`,
     });
