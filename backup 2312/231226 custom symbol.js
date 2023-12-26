b318a433f403fdcecd942aca3983e663c52ba41a
Nata3951 <Nata3951@gmail.com>
2023-12-26 17:04:46 +0300

    Update 231226 custom symbol.js

--- backup 2312/231226 custom symbol.js
+++ backup 2312/231226 custom symbol.js
@@ -1,3 +1,62 @@
+
+// Define a custom symbol path
+Highcharts.SVGRenderer.prototype.symbols.rect = function (x, y, w, h) {
+return [
+'M', x, y+h/2,
+'L', x+w, y+h/2,
+'z'];
+};
+
+
+if (Highcharts.VMLRenderer) {
+Highcharts.VMLRenderer.prototype.symbols.rect = Highcharts.SVGRenderer.prototype.symbols.rect;
+}
+
+
+Highcharts.chart({
+    chart: {
+        renderTo: w.general.renderTo
+    },
+    legend: {
+        enabled: true,
+        verticalAlign: 'top',
+        margin : 15,
+    },
+    xAxis: {
+        categories: w.data.rows,
+        gridLineColor: line_color,
+        gridLineWidth: 1,
+    },
+
+    tooltip: {
+        shared: true
+    },
+    
+    plotOptions : {
+         
+        spline: {
+          lineWidth: 0,
+          marker: {
+             symbol: 'rect',
+             lineWidth: 4,
+             radius: 8,
+          },
+        },
+    },
+
+   series: [
+   //  ...
+        {
+            name: 'B+',
+            type: 'spline',
+            color: yellow,
+            data: get_forecast('B'),
+            pointPlacement : -0.2,
+            marker: {
+            lineColor: yellow_dark,
+          },
+
+//  ----------------------------------------------------------------------------
 // Создаём path контейнер для будущего кастомного маркера  
 if (!Highcharts.SVGRenderer.prototype.symbols.dottedLine) {
     Highcharts.SVGRenderer.prototype.symbols.dottedLine = function (x, y, w, h) {
