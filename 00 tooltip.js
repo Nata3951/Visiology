e6306542377f31059d15219f1e0bd30ac2fb82d9
Nata3951 <Nata3951@gmail.com>
2024-01-16 11:31:17 +0300

    Update 00 tooltip.js

--- 00 tooltip.js
+++ 00 tooltip.js
@@ -47,3 +47,34 @@ const chart = Highcharts.chart({
         shared: true,
         useHTML: true
     },
+
+// fan chart - area range 
+
+    tooltip: {
+        shared: true,
+        useHTML: true,
+        formatter: function(e) {
+            console.log(e)
+            console.log(this)
+            let str = `
+            <span style="font-size:12px;color:#757575;font-family:Open Sans">${this.points[0].key}</span><table>
+            <tr><td style="font-size:12px;font-family:Open Sans;color:${this.points[0].series.color};padding:10">${this.points[0].series.name}: </td>
+            <td style="padding:0;text-align:right"><b style="font-size:13px;color:#212121;font-family:Open Sans">${this.points[0].y.toFixed(2)}</b></td></tr>
+            <tr><td style="font-size:12px;font-family:Open Sans;color:${this.points[1].series.color};padding:10">${this.points[1].series.name}: </td>
+            <td style="padding:0;text-align:right"><b style="font-size:13px;color:#212121;font-family:Open Sans">${this.points[1].y.toFixed(2)}</b></td></tr>
+            `
+            
+            if (forecast[this.points[0].point.index][1]) {
+                str += `
+                    <tr><td style="font-size:12px;font-family:Open Sans;color:${this.points[0].series.color};padding:10">Диапазон с вероятностью 68%: </td>
+                    <td style="padding:0;text-align:right"><b style="font-size:13px;color:#212121;font-family:Open Sans">${forecast[this.points[0].point.index][1].toFixed(2)} - ${forecast[this.points[0].point.index][2].toFixed(2)}</b></td></tr>
+                    </table>
+                `
+            } else {
+                str += `
+                    </table>
+                `
+            }
+            
+            return str
+        }
