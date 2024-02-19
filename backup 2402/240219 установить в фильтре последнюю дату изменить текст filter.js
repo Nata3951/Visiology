52a0d28123ca2ebc8f334ec52dc269959c74047d
Nata3951 <Nata3951@gmail.com>
2024-02-19 10:23:21 +0300

    Update and rename 240219 установить в фильтре последнюю имеющуюся дату.js to 240219 установить в фильтре последнюю дату изменить текст filter.js

---
+++ backup 2402/240219 установить в фильтре последнюю дату изменить текст filter.js
@@ -0,0 +1,26 @@
+let unit = "квартал ";
+
+// добавим текст к второму уровню фильтра
+w.data.data.forEach(data => {
+    data.children.forEach(child =>{
+        child.text = unit + child.text;
+    });
+});
+
+FilterRender({
+    filter: w.general,
+    style: w.style,
+    textStyle: w.textStyle,
+    data: w.data
+});
+
+
+if (w.data.selected.length < 1){
+    visApi().getWidgetDataByGuid(w.general.renderTo).then(function (widgetData) {
+        // console.log('test ', widgetData.data.rows.at(-1));
+        let lastPeriod = widgetData.data.rows.at(-1);
+        visApi().setFilterSelectedValues(w.general.renderTo, [lastPeriod], function (response) {});
+        $('#' + w.general.renderTo + ' .rb-filter-header-text span').text(lastPeriod[0] + " " + unit +" " + lastPeriod[1]);
+    });
+}
+
