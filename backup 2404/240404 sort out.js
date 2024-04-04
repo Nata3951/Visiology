25dbcfab33d4e82e7326931d929c0f480e9faf79
Nata3951 <Nata3951@gmail.com>
2024-04-04 12:08:18 +0300

    Create 240404 sort out.js

---
+++ backup 2404/240404 sort out.js
@@ -0,0 +1,7 @@
+let filterDealGuid = 'cb9b34f60dc245578afe1d80c48f55c4'; 
+let filterContractGuid = '85026369d28d4b4c95bc96ad81360ef3'; 
+let filterVersionGuid = '094d5297c51d4a899bc4e9e3d8e4d28e';
+
+let dealSelected = visApi().getSelectedValues(filterDealGuid);
+let contractSelected = visApi().getSelectedValues(filterContractGuid);
+let versionSelected = visApi().getSelectedValues(filterVersionGuid);
